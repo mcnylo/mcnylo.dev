bd76fa0a7d513aabc2c772a718d86fcd86c87d25
mcnylo <michaelcmcniel@gmail.com>
2026-08-17 22:33:31 -0500

    Changing theme from blue to green

--- mcnylo.dev/wwwroot/js/site.js
+++ mcnylo.dev/wwwroot/js/site.js
@@ -1,1 +1,27 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const copyEmailButton = document.getElementById("copy-email");
+    const copyEmailToast = document.getElementById("copy-email-toast");
+
+    if (!copyEmailButton || !copyEmailToast) return;
+
+    let toastTimeout;
+
+    copyEmailButton.addEventListener("click", async () => {
+        const email = `${copyEmailButton.dataset.emailUser}@${copyEmailButton.dataset.emailDomain}`;
+
+        try {
+            await navigator.clipboard.writeText(email);
+
+            copyEmailToast.classList.remove("hidden");
+
+            clearTimeout(toastTimeout);
+
+            toastTimeout = setTimeout(() => {
+                copyEmailToast.classList.add("hidden");
+            }, 2500);
+        }
+        catch {
+            console.error("Unable to copy email address to clipboard.");
+        }
+    });
+});
